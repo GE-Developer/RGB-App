ae2ba2867c7327bb4b2815f2213a13b64e45d2c6
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-04-09 00:07:25 +0300

    Initial Commit

---
+++ RGB App/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  RGB App
+//
+//  Created by Mikhail Bukhrashvili on 26.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - IB Outlets
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        setColor()
+        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+    }
+    
+    // Изменение цветов слайдерами
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+                
+        switch sender {
+        case redSlider: setValue(for: redValueLabel)
+        case greenSlider: setValue(for: greenValueLabel)
+        default: setValue(for: blueValueLabel)
+        }
+    }
+    
+    // Цвет вью
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                label.text = string(from: redSlider)
+            case greenValueLabel:
+                label.text = string(from: greenSlider)
+            default:
+                label.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    // Значения RGB
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
